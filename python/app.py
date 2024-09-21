ae6cb036e2aaa3f5fcbf045acd27df24bcf7c832
K3reem1 <kareemtantawy01@gmail.com>
2024-09-21 23:51:50 +0300

    Updated the app.py content

---
+++ python/app.py
@@ -0,0 +1,8 @@
+day = 28
+month = 6; year = 2001
+firstName = "Kareem"
+lastName = "Tantawy"
+graduationYear = 2024
+print("Name:",firstName,lastName)
+print("Birth Date:",day,'/',month,'/',year)
+print ("Graduation Year:",graduationYear)
