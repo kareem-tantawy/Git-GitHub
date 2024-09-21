2f0a0b75fc80a7404153c6014a04538db79e93e5
K3reem1 <kareemtantawy01@gmail.com>
2024-09-22 00:30:16 +0300

    Updated the app.py content

--- python/app.py
+++ python/app.py
@@ -1,11 +1,13 @@
 day = 28
-month = 6; year = 2001
+month = 6
+year = 2001
 firstName = "Kareem"
 lastName = "Tantawy"
 graduationYear = 2024
 bio = "Junior Civil Engineer | Structural Analysis & Design Specialist"
 linkedInUser = "kareem-ahmed-tantawy"
-print("Name:",firstName,lastName)
-print("Birth Date:",day,'/',month,'/',year)
-print ("Graduation Year:",graduationYear)
-print("Bio:", bio); print("Linked In profile User name:", linkedInUser)
+print("Name:", firstName, lastName)
+print("Birth Date:", day, "/", month, "/", year)
+print("Graduation Year:", graduationYear)
+print("Bio:", bio)
+print("Linked In profile User name:", linkedInUser)
